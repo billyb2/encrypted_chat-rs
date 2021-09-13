f0547fd38caf3f9fba0d4f53945b008debdae524
William Batista <bootlegbilly@protonmail.ch>
2021-09-13 21:35:31 +0000

    Added a basic UI
    The UI does just about everything I want it to do, albeit being slightly more complicated than I'd like it to be.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use base64::{encode_config, decode_config};
-
 use smaz::{compress, decompress};
+
+use base64::{encode_config, decode_config};
 
 use wasm_bindgen::prelude::*;
 
@@ -15,8 +15,8 @@ pub fn main() -> Result<(), JsValue> {
     Ok(())
 }
 
-#[wasm_bindgen]
 //TODO: Do precompute stuff
+#[wasm_bindgen]
 pub fn gen_priv_key() -> String {
     let mut rng = OsRng;
     let priv_key = RsaPrivateKey::new(&mut rng, BIT_SIZE).expect("failed to generate a key");
@@ -28,7 +28,6 @@ pub fn gen_priv_key() -> String {
 
 #[wasm_bindgen]
 pub fn gen_pub_key(priv_key: String) -> String {
-
     let public_key = RsaPrivateKey::from_pkcs1_pem(&priv_key).unwrap().to_public_key();
     public_key.to_public_key_pem().unwrap()
 
@@ -50,13 +49,23 @@ pub fn enc(unencrypted_string: String, pub_key: String) -> String {
 
 #[wasm_bindgen]
 pub fn dec(encrypted_string: String, priv_key: String) -> String {
-    let encrypted_compressed_bytes = decode_config(encrypted_string, base64::URL_SAFE_NO_PAD).unwrap();
-
     let priv_key = RsaPrivateKey::from_pkcs1_pem(&priv_key).unwrap();
+    
+    let encrypted_compressed_bytes = decode_config(encrypted_string, base64::URL_SAFE_NO_PAD).unwrap();
     let unencrypted_compressed_bytes = priv_key.decrypt(PADDING, &encrypted_compressed_bytes).unwrap();
 
     let string_bytes = decompress(&unencrypted_compressed_bytes).unwrap();
 
     String::from_utf8_lossy(&string_bytes).to_string()
 
+}
+
+#[wasm_bindgen]
+pub fn b64_encode(string: String) -> String {
+    encode_config(string, base64::URL_SAFE_NO_PAD)
+}
+
+#[wasm_bindgen]
+pub fn b64_decode(string: String) -> String {
+    String::from_utf8_lossy(&decode_config(string, base64::URL_SAFE_NO_PAD).unwrap()).to_string()
 }
